3771fb49f43f64326be5035e95b0e04e6788e650
TobbyJay <umohjunior96@gmail.com>
2021-01-30 15:21:47 +0100

    user interface for Edit page complete

---
+++ TradingRitual.Service/Interface/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingRitual.Entities.Models;
+
+namespace TradingRitual.Service.Interface
+{
+    public interface IUserService
+    {
+        public Profile GetUserByEmail(string email);
+    }
+}
